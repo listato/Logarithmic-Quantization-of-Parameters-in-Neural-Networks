8b8a1d0976dccd3a0c569e2fa4ec3cef27306e4a
Jingyong Cai <1769395594@qq.com>
2018-09-04 23:14:09 +0900

    Update computeAccuracy.py

--- quantizationTools/computeAccuracy.py
+++ quantizationTools/computeAccuracy.py
@@ -1,1 +1,16 @@
+from chainer.dataset import concat_examples
+import chainer.functions as F
 
+
+class computeAccuracy:
+    def __init__(self, iter, model):
+        self.iteration = iter.next()
+        self.model = model
+
+
+    @property
+    def accuracy(self):
+        image_test, target_test = concat_examples(self.iteration, -1)
+        prediction_test = self.model(image_test)
+        accuracy = F.accuracy(prediction_test, target_test)
+        return accuracy.data
