62d97b8133a93e4767b75b6eabbb2bc3e4f22b5f
Jingyong Cai <1769395594@qq.com>
2018-09-07 18:12:37 +0900

    Update googlenet.py

--- benchmark/benchgoogle/googlenet.py
+++ benchmark/benchgoogle/googlenet.py
@@ -1,24 +1,27 @@
 from chainer.links import GoogLeNet
 import numpy as np
-
-
-from PIL import Image
+import multiprocessing as mp
 import glob
-image_list = []
-for filename in glob.glob('D:\BaiduNetdiskDownload\ILSVRC2012_img_val\*.JPEG'):
-    im=Image.open(filename)
-    #im = vgg.prepare(im)
-    # print(im)
-    image_list.append(im)
+from PIL import Image
 
-print('ok')
-
-goo = GoogLeNet()
-
-y = goo.predict(image_list)
-y1 = np.array(y.data)
-print(y1.shape)
-for i in y1:
-    print(np.argmax(i))
-print(image_list)
-print()
+def main():
+    image_list = []
+    for filename in glob.glob('D:\BaiduNetdiskDownload\ILSVRC2012_img_val\*0000000*.JPEG'):
+        im=Image.open(filename)
+        #im = vgg.prepare(im)
+        # print(im)
+        image_list.append(im)
+    print('ok')
+    goo = GoogLeNet()
+    y = goo.predict(image_list)
+    y1 = np.array(y.data)
+    print(y1.shape)
+    y2 = np.array([])
+    for i in y1:
+        x = np.argmax(i)
+        print(x)
+        y2 = np.append(y2, x)
+    print(image_list)
+    #np.savetxt('D:\BaiduNetdiskDownload\computed.txt', y2 , fmt='%u')
+if __name__=='__main__':
+    main()
