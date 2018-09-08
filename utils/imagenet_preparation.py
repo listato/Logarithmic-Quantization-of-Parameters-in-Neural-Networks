175a986af877c01f79b4105c0684f3186a3d79c8
Jingyong Cai <1769395594@qq.com>
2018-09-09 01:11:41 +0900

    Update imagenet_preparation.py

--- utils/imagenet_preparation.py
+++ utils/imagenet_preparation.py
@@ -6,39 +6,7 @@ from __future__ import division
 import numpy
 import glob
 from PIL import Image
-
-def prepare(image, size=(256, 256)):
-    """Converts the given image to the numpy array for GoogLeNet.
-        Note that you have to call this method before ``__call__``
-    because the pre-trained GoogLeNet model requires to resize the given
-    image, covert the RGB to the BGR, subtract the mean,
-    and permute the dimensions before calling.
-    Args:
-    image (PIL.Image or numpy.ndarray): Input image.
-            If an input is ``numpy.ndarray``, its shape must be
-            ``(height, width)``, ``(height, width, channels)``,
-            or ``(channels, height, width)``, and
-            the order of the channels must be RGB.
-        size (pair of ints): Size of converted images.
-            If ``None``, the given image is not resized.
-    Returns:
-        numpy.ndarray: The converted output array.
-    """
-    if isinstance(image, numpy.ndarray):
-        if image.ndim == 3:
-            if image.shape[0] == 1:
-                image = image[0, :, :]
-            elif image.shape[0] == 3:
-                image = image.transpose((1, 2, 0))
-        image = Image.fromarray(image.astype(numpy.uint8))
-    image = image.convert('RGB')
-    if size:
-        image = image.resize(size)
-    image = numpy.asarray(image, dtype=numpy.float32)
-    image = image[:, :, ::-1]
-    image -= numpy.array([104.0, 117.0, 123.0], dtype=numpy.float32)  # BGR
-    image = image.transpose((2, 0, 1))
-    return image
+from chainer.links.model.vision.googlenet import prepare
 
 def create_datasets(groups=500, imgpath='', savepath=''):
 #def create_datasets(groups=500, imgpath='D:\BaiduNetdiskDownload\ILSVRC2012_img_val\\', savepath='E:\ccc\imgroup'):
@@ -54,7 +22,7 @@ def create_datasets(groups=500, imgpath='', savepath=''):
     for index, namelists in enumerate(img_lists[0:10]):
         for jpgnames in namelists:
             im = Image.open(jpgnames)
-            tmp_lists[index].append(prepare(im).tolist())
+            tmp_lists[index].append(prepare(im, size=(256,256)).tolist())
             im.close()
         numpy.save(savepath + str(index), numpy.array(tmp_lists[index], dtype=numpy.float32))
         tmp_lists[index] = []
