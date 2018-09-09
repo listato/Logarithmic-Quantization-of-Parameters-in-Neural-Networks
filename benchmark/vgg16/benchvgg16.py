c756a83aaf15446e8d4fccf88a65b9cbd656dd8d
Jingyong Cai <1769395594@qq.com>
2018-09-09 18:37:48 +0900

    Update benchvgg16.py

--- benchmark/vgg16/benchvgg16.py
+++ benchmark/vgg16/benchvgg16.py
@@ -1,19 +1,21 @@
 from chainer.links.model.vision import vgg
 import numpy as np
-import multiprocessing as mp
-from pathos.multiprocessing import ProcessingPool as Pool
+from pathos.multiprocessing import ThreadingPool as Pool
+
+
 
 def main():
     v16 = vgg.VGG16Layers()
 
     #print([a])
     y = []
-    for i in range(500):
-        pool = Pool(5)
+    pool = Pool(16)
+    for i in range(10):
         #[np.load('/home/cjy/cjy/imnet/imgroup' + str(i) + '.npy').tolist() for i in range(4*i,4*i+4)])
         x = np.load('/home/cjy/cjy/imnet/imgroup' + str(i) + '.npy')
-        y.append(pool.map(v16.predict, [x[_:(_+10)] for _ in np.arange(100,step=10)]))
-        pool.close()
+        t = [x[_:(_+1)] for _ in np.arange(100)]
+        y.append(pool.map(v16.predict, t))
+        #pool.close()
 
     result = []
     for z in y:
@@ -24,7 +26,7 @@ def main():
 
     print(np.array(result).shape)
 
-    np.save('/home/cjy/cjy/imnet/imgroup/vgg16', np.array(result, dtype=np.int32))
+    np.save('/home/cjy/cjy/result/vgg16', np.array(result, dtype=np.int32))
 
 if __name__ == "__main__":
     main()
