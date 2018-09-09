efad5e68c938c79bb454591e59e54bfed79a20fb
Jingyong Cai <1769395594@qq.com>
2018-09-09 17:21:52 +0900

    Update benchvgg16.py

--- benchmark/vgg16/benchvgg16.py
+++ benchmark/vgg16/benchvgg16.py
@@ -1,16 +1,20 @@
 from chainer.links.model.vision import vgg
 import numpy as np
 import multiprocessing as mp
-
+from pathos.multiprocessing import ProcessingPool as Pool
 
 def main():
     v16 = vgg.VGG16Layers()
 
+    #print([a])
     y = []
-    for i in range(125):
-        pool = mp.Pool(4)
-        y.append(pool.map(v16.predict, [np.load('E:\imagenet\imgroup' + str(i) + '.npy') for i in range(4*i,4*i+4)]))
+    for i in range(500):
+        pool = Pool(5)
+        #[np.load('/home/cjy/cjy/imnet/imgroup' + str(i) + '.npy').tolist() for i in range(4*i,4*i+4)])
+        x = np.load('/home/cjy/cjy/imnet/imgroup' + str(i) + '.npy')
+        y.append(pool.map(v16.predict, [x[_:(_+10)] for _ in np.arange(100,step=10)]))
         pool.close()
+
     result = []
     for z in y:
         for zz in z:
@@ -20,7 +24,7 @@ def main():
 
     print(np.array(result).shape)
 
-    np.save('D:\BaiduNetdiskDownload\\result\\vgg16.txt', np.array(result, dtype=np.int32))
+    np.save('/home/cjy/cjy/imnet/imgroup/vgg16', np.array(result, dtype=np.int32))
 
 if __name__ == "__main__":
     main()
