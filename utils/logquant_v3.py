59140b90fe224b51f83483eee2d1a06d18aa1e5c
Jingyong Cai <1769395594@qq.com>
2018-09-19 01:27:40 +0900

    Update logquant_v3.py

--- utils/logquant_v3.py
+++ utils/logquant_v3.py
@@ -9,20 +9,18 @@ class LogQuant3:
     def __init__(self,layer,bitwidth=4):
         self.layer_data = layer
         self.width = bitwidth
-        self.maxima = numpy.amax(layer)
-        self.minima = numpy.amin(layer)
         self.sign = numpy.sign(layer)
         self.lookup = numpy.linspace(0,-7,2**self.width)
-
+        
     def __round(self,x):
         idx = (numpy.abs(self.lookup - x)).argmin()
         return idx
-
+    
     @property
     def log_quantized(self):
         round = numpy.vectorize(self.__round)
         return numpy.array(round(numpy.log2(numpy.abs(self.layer_data))),dtype=numpy.int8)
-
+    
     @property
     def de_quantized(self):
         x = numpy.power(2.0, self.lookup[self.log_quantized])
