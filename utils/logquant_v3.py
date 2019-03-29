dbc6a48ab5e0bf4361be459a45598523f2344371
Jingyong Cai <1769395594@qq.com>
2019-03-29 23:58:43 +0900

    Update logquant_v3.py

--- utils/logquant_v3.py
+++ utils/logquant_v3.py
@@ -1,7 +1,10 @@
 """
 author: CAI JINGYONG@BeatCraft Inc.
+by far one of the best quantization algorithms for neural networks quantization without retraining.
 
-by far the best one for neural networks quantization
+log_quantized is the quantized weights which need to be stored.
+
+de_quantized which replaces the original weights needs to be implemented in target devices.
 """
 import numpy
 class LogQuant:
@@ -13,9 +16,11 @@ class LogQuant:
     def __round(self,x):
         idx = (numpy.abs(self.lookup - x)).argmin()
         return idx
+    @property
     def log_quantized(self):
         round = numpy.vectorize(self.__round)
         return numpy.array(round(numpy.log2(numpy.abs(self.layer_data))),dtype=numpy.int8)
+    @property
     def de_quantized(self):
         x = numpy.power(2.0, self.lookup[self.log_quantized])
         x = numpy.array(x,dtype=numpy.float32)
